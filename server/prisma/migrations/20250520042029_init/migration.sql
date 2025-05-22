711b4d502b1e99ba97eabcd2bb132b1469cbe86c
Trong Anh Nguyen Thanh <tronganhnguyen222@gmail.com>
2025-05-22 19:48:57 +0700

    [UPDATE][FE] fixed google font

--- server/prisma/migrations/20250520042029_init/migration.sql
+++ server/prisma/migrations/20250520042029_init/migration.sql
@@ -4,5 +4,5 @@ CREATE TABLE "posts" (
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "authorId" INTEGER NOT NULL,
-    CONSTRAINT "posts_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "user" ("userId") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "posts_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "user" ("userId")
 );
