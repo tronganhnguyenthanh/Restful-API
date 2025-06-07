8ae7d34bce359941e88814bdec008fc673022e06
Trong Anh Nguyen Thanh <tronganhnguyen222@gmail.com>
2025-06-07 12:56:07 +0700

    [UPDATE][FE] build loading for view detail posts

--- client/src/components/formPostDetail/PostDetail.js
+++ client/src/components/formPostDetail/PostDetail.js
@@ -1,20 +1,23 @@
 import axios from "axios"
-import {useEffect} from "react"
-import {useState} from "react"
-import {Card, Col, Container, Row} from "react-bootstrap"
-import {Link, useParams} from "react-router-dom"
+import { useEffect } from "react"
+import { useState } from "react"
+import { Card, Col, Container, Row } from "react-bootstrap"
+import { Link, useParams } from "react-router-dom"
+import Loading from "../../loading/Loading"
 const PostDetail = () => {
-  const {userId} = useParams()
+  const { userId } = useParams()
   const [userPostsDetail, setUserPostsDetail] = useState({})
   const [userPosts, setUserPosts] = useState([])
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
-   getUserPostsDetail(userId)
-  },[userId])
+    getUserPostsDetail(userId)
+  }, [userId])
   const getUserPostsDetail = async (userId) => {
-   let res = await axios.get(`https://rest-api-server-1-iwtx.onrender.com/user/post/detail/${userId}`)
-   let posts = await res.data.posts
-   setUserPosts(posts.post)
-   setUserPostsDetail(posts)
+    let res = await axios.get(`https://rest-api-server-1-iwtx.onrender.com/user/post/detail/${userId}`)
+    let posts = await res.data.posts
+    setUserPosts(posts.post)
+    setUserPostsDetail(posts)
+    setLoading(!loading)
   }
   return (
     <Container className="p-2">
@@ -24,21 +27,45 @@ const PostDetail = () => {
             <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
           </svg>
         </Link>
-        <h2 className="text-center text-primary m-2">User's detail post list ({userPostsDetail.firstName} {userPostsDetail.lastName})</h2>
+        <h2 className="text-center text-primary m-2">
+          User's detail post list
+          <>
+           {
+            loading 
+            ? 
+            <Loading/> 
+            :
+            <>
+             ({userPostsDetail.lastName} 
+             {userPostsDetail.firstName})
+            </>
+           }
+          </>
+        </h2>
       </div>
       <Row className="p-2">
-        <p className="text-primary"></p>
-        {userPosts.length > 0 && userPosts.map((item, index) => {
-          return (
-            <Col lg="12" key={index}>
-              <Card className="p-2 m-2">
-                <h2 className="text-center text-info">{item.title}</h2>
-                <span className="text-center text-secondary">{item.content}</span>
-              </Card>
-            </Col>
-          )
-        })
+        <>
+        {
+         loading ?
+          <div>
+            <Loading/>
+          </div>
+          :
+          <>
+           {userPosts.length > 0 && userPosts.map((item, index) => {
+             return (
+               <Col lg="12" key={index}>
+                 <Card className="p-2 m-2">
+                   <h2 className="text-center text-info">{item.title}</h2>
+                   <span className="text-center text-secondary">{item.content}</span>
+                 </Card>
+               </Col>
+             )
+            })
+           }
+          </>
         }
+        </>
       </Row>
     </Container>
   )
